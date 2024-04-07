7427a1272afaa363e76208c99225771dbf10e2a7
jefflealarrom <jefflealarrom@gmail.com>
2024-04-07 14:24:01 +0200

    feat(edicion del archivo api js y creacion de interface

---
+++ src/app/interfaces/products.—type=“interface”.ts
@@ -0,0 +1,12 @@
+export interface Products {
+    createdAt: string,
+    name: string,
+    img_one: string,
+    img_two: string;
+    img_three: string;
+    info: string;
+    price: string;
+    gender: string;
+    brand: string;
+    id: string
+}
