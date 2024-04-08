26ddd7df6683ecaf3416a5a92a2fce4cb8d6ac41
jefflealarrom <jefflealarrom@gmail.com>
2024-04-08 19:16:08 +0200

    feat(llamada a la api desde el servidor all products y pintado en el front)

--- src/app/components/all-products/all-products.component.ts
+++ src/app/components/all-products/all-products.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AllProductosService } from 'src/app/services/all-productos.service';
+import { Component, OnInit, inject } from '@angular/core';
+import { Products } from 'src/app/interfaces/products.—type=“interface”';
 
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
   styleUrls: ['./all-products.component.css']
 })
-export class AllProductsComponent {
+export class AllProductsComponent implements OnInit {
 
+  products: Products[] = [];
+  productService = inject(AllProductosService)
+
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+  fetchPosts(): void {
+    this.productService.getAll().subscribe((
+      data: Products[]) => {
+      this.products = data;
+    },
+      (error) => {
+        console.error('Error al obtener los productos:', error);
+      }
+    );
+  }
 }
