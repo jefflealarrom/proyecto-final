13198541e02314ea0a2bfc69c44add23638cd120
jefflealarrom <jefflealarrom@gmail.com>
2024-04-09 14:21:26 +0200

    feat(correccion del filtrado por id)

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,5 +1,5 @@
-import { Products } from 'src/app/interfaces/products.—type=“interface”';
 import { Component, inject } from '@angular/core';
+import { Products } from 'src/app/interfaces/products.—type=“interface”';
 import { AllProductosService } from 'src/app/services/all-productos.service';
 
 @Component({
@@ -9,16 +9,13 @@ import { AllProductosService } from 'src/app/services/all-productos.service';
 })
 export class DetailsComponent {
 
-  products: Products[] = [];
+  products: Products [] = [];
   servicio = inject(AllProductosService)
 
-  ngOnInit(): void{
-
-    this.servicio.getId('1').subscribe((
-      data: Products[]) => {
-        console.log(data);
-        this.products = data;
-      }
-    )
+  ngOnInit(): void {
+    this.servicio.getId('1').subscribe((data: Products) => {
+      console.log(data);
+      this.products = [data];
+    });
   }
 }
