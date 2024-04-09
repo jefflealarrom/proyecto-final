7ac85802b79a05c48519395cfe991b967cdb32bb
jefflealarrom <jefflealarrom@gmail.com>
2024-04-09 17:46:27 +0200

    feat(correcion btn detail)

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Products } from 'src/app/interfaces/products.—type=“interface”';
 import { AllProductosService } from 'src/app/services/all-productos.service';
 
@@ -11,11 +12,18 @@ export class DetailsComponent {
 
   products: Products [] = [];
   servicio = inject(AllProductosService)
+  router = inject(ActivatedRoute)
+  
 
   ngOnInit(): void {
-    this.servicio.getId('1').subscribe((data: Products) => {
-      console.log(data);
-      this.products = [data];
-    });
+    const id = this.router.snapshot.paramMap.get('id');
+    if (id !== null) {
+      this.servicio.getId(id).subscribe((data: Products) => {
+        console.log(data);
+        this.products = [data];
+      });
+    } else {
+      console.error('ID del producto no encontrado en la URL');
+    }
   }
 }
