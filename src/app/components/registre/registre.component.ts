0b124aa05a64cb2c62d904a5a26bbcad3e02fccd
Jhon <jhonda82@hotmail.com>
2024-04-10 12:14:09 +0200

    primer commit

--- src/app/components/registre/registre.component.ts
+++ src/app/components/registre/registre.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-@Component({
-  selector: 'app-registre',
-  templateUrl: './registre.component.html',
-  styleUrls: ['./registre.component.css']
-})
+@Component({   
+  selector: 'app-registre', 
+  templateUrl: './registre.component.html', 
+  styleUrls: ['./registre.component.css'] })
 export class RegistreComponent {
+
+  registro: FormGroup;
+  constructor(){
+    this.registro=new FormGroup({
+      nombre: new FormControl(),
+      apellidos: new FormControl(),
+      correoElectronico: new FormControl("",[
+        Validators.required,
+        Validators.email
+      ]),
+      contraseña: new FormControl("",
+      Validators.minLength(6)
+
+      ),
+      confirmarContraseña: new FormControl("",
+        Validators.minLength(6)
+      )
+    })
+  }
+
+  onSubmit() {
+    console.log("enviando datos");
+  
+  }
 
 }
