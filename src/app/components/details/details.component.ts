d425cb32ca6a6063c46d5aa85243d5ea982c3b04
jefflealarrom <jefflealarrom@gmail.com>
2024-04-11 13:52:58 +0200

    feat(cambios css)

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,3 @@
-import { ShopService } from './../../services/shop.service';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute,  } from '@angular/router';
 import { Products } from 'src/app/interfaces/products.—type=“interface”';
@@ -11,7 +10,7 @@ import { AllProductosService } from 'src/app/services/all-productos.service';
 })
 export class DetailsComponent {
 
-  products!: Products;
+  products: Products [] = [];
   servicio = inject(AllProductosService)
   router = inject(ActivatedRoute)
   index: number = 0;
@@ -21,8 +20,8 @@ export class DetailsComponent {
     const id = this.router.snapshot.paramMap.get('id');
     if (id !== null) {
       this.servicio.getId(id).subscribe((data: Products) => {
-        this.products = data;
-        console.log(this.products);
+        console.log(data);
+        this.products = [data];
       });
     } else {
       console.error('ID del producto no encontrado en la URL');
@@ -30,24 +29,18 @@ export class DetailsComponent {
   }
 
   nextImage() {
-    // const product = this.products[0];
-    // const numImages = [product.img_one, product.img_two, product.img_three].filter(img => img).length;
-    // if (this.index < numImages - 1) {
+    const product = this.products[0];
+    const numImages = [product.img_one, product.img_two, product.img_three].filter(img => img).length;
+    if (this.index < numImages - 1) {
       this.index++;
-    // }
+    }
   }
 
   prevImage() {
-    // if (this.index > 0) {
+    if (this.index > 0) {
       this.index--;
-    // }
-  }
-  constructor(private ShopService: ShopService) { }
-
-  addToCart(product: any) {
-    this.ShopService.addToCart(product);
-    console.log(ShopService);
-    
+    }
   }
 
 }
+
