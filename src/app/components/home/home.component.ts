954a89c669c65a2f7c16766cdd4362ff06176ad3
NereaRVA <nerea.rva1@gmail.com>
2024-04-15 13:42:57 +0200

    cssgeneral

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.http.get<any[]>('https://661270f095fdb62f24eeaffd.mockapi.io/api/perfumes/products')
+      .subscribe(data => {
+        this.products = data.slice(0, 4);
+        console.log(this.products);
+      });
+  }
 }
